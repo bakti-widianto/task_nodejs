227da28ae1e83211b1d8159e271faf9ca5791d74
bakti-widianto <bakti.widianto@gmail.com>
2020-08-16 02:22:10 +0700

    remind done

--- routes/task.js
+++ routes/task.js
@@ -4,25 +4,33 @@ var moment = require('moment');
 
 module.exports = (db) => {
   router.get('/', function (req, res, next) {
-    const sql = `SELECT * FROM task LEFT JOIN tag ON task.id = tag.id`
+    let remind = []
+    const sql = `SELECT * FROM task LEFT JOIN tag ON task.id = tag.id ORDER BY date ASC`
     db.query(sql, (err, data) => {
       if (err) return res.status(500).json({
         error: true,
         message: err
       })
-      console.log(data.rows[1].starttime)
       for (let i = 0; i < data.rows.length; i++) {
-        data.rows[i].date = moment(data.rows[i].date).format('MMM Do YY')
+        data.rows[i].date = moment(data.rows[i].date).format('MMM Do YYYY')
 
-        
+        if (data.rows[i].date == moment().format('MMM Do YYYY')) {
+          remind.push(data.rows[i])
+        }
       }
       res.json({
         error: false,
-        data: data.rows
+        data: data.rows,
+        remind
       })
     })
-
   });
+
+
+
+
+
+
 
   return router;
 }
