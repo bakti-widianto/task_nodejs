9387379f8354ececfcbdb5820fa0f6f5eca15b5c
bakti-widianto <bakti.widianto@gmail.com>
2020-08-17 02:57:04 +0700

    add done

--- routes/task.js
+++ routes/task.js
@@ -26,6 +26,53 @@ module.exports = (db) => {
     })
   });
 
+  router.post('/', function (req, res) {
+    let task = req.body.task.split(' ')
+    let time = ''
+    let date = ''
+    let result = ''
+
+    task.forEach(item => {
+      //time
+      for (let i = 0; i <= 12; i++) {
+        if (item == `${i}pm`) {
+          time = item
+        } else if (item == `${i}am`) {
+          time = item
+        }
+      }
+      //date
+      for (let x = 0; x <= 12; x++) {
+        for (let y = 0; y <= 31; y++) {
+
+          if (item == 'tomorrow') {
+            date = moment().add(1, 'days').format('YYYY-MM-DD')
+
+          } else if (item == `2020-${x}-${y}`) {
+            date = item
+          }
+        }
+      }
+      //task
+      if (item !== time && item !== date && item !== 'tomorrow') {
+        result += item + ' '
+      }
+    });
+
+
+    let sql = `INSERT INTO task(task, time, date) VALUES($1,$2,$3)`
+    let values = [result, time, date]
+    db.query(sql, values, (err) => {
+      if (err) return res.status(500).json({
+        error: true,
+        message: err
+      })
+      res.send('ADD SUCCESS')
+    })
+  })
+
+
+
 
 
 
